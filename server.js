843b8439a6ded34f86f859b6788812e18752060f
lfrendahl <lfrendahl3@gmail.com>
2023-06-26 13:12:59 -0400

    finished changing syntax for all the routes in async await format

--- server.js
+++ server.js
@@ -39,7 +39,79 @@ app.post('/addPlayer', async (req, res) => {
     })
 
 
-/*
+
+//UPDATE - RAISE score
+app.put('/addOneStroke', async (req, res) => {
+    await client.db('DiscGolfScoreKeeper')
+                        .collection('players')
+                        .updateOne({playerName: req.body.playerNameCur, score: req.body.scoreCur},{
+        $set: {
+            score:req.body.scoreCur + 1
+        }
+    },{
+       sort: {_id: -1},
+       upsert: true 
+    })
+    
+        console.log('Added a stroke to score')
+        res.json('Stroke added')
+})
+
+
+//UPDATE - LOWER score
+app.put('/removeOneStroke', async (req, res) => {
+    await client.db('DiscGolfScoreKeeper')
+                    .collection('players')
+                    .updateOne({playerName: req.body.playerNameCur, score: req.body.scoreCur},{
+        $set: {
+            score:req.body.scoreCur - 1
+        }
+    },{
+       sort: {_id: -1},
+       upsert: true 
+    })
+        console.log('Removed a stroke from score')
+        res.json('Stroke removed')
+    })
+
+//DELETE: deleteOne
+app.delete('/deletePlayer', async(req, res) => {
+    await client.db('DiscGolfScoreKeeper')
+                    .collection('players')
+                    .deleteOne({playerName: req.body.playerNameCur || ''})
+        console.log('Player deleted')
+        res.json('Player deleted')
+})
+
+
+
+
+
+
+
+
+/*OLD CONNECT
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'DiscGolfScoreKeeper'
+
+    app.get('/',(request, response) =>{
+    db.collection('players').find().sort({score: 1}).toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data})
+    })
+    .catch(error => console.error(error))
+})
+//CREATE /addPlayer 
+app.post('/addPlayer', (request, response) => {
+    db.collection('players').insertOne({playerName: request.body.playerName, score: 0})
+    .then(result => {
+        console.log('New player added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
 //UPDATE - RAISE score
 app.put('/addOneStroke', (request, response) => {
     db.collection('players').updateOne({playerName: request.body.playerNameCur, score: request.body.scoreCur},{
@@ -74,45 +146,6 @@ app.put('/removeOneStroke', (request, response) => {
     .catch(error => console.error(error))
 })
 
-//DELETE: deleteOne
-app.delete('/deletePlayer', (request, response) => {
-    db.collection('players').deleteOne({playerName: request.body.playerNameCur || ''})
-    .then(result => {
-        console.log('Player deleted')
-        response.json('Player deleted')
-    })
-    .catch(error => console.error(error))
-})
-*/
-
-
-
-
-
-
-
-/*OLD CONNECT
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'DiscGolfScoreKeeper'
-
-    app.get('/',(request, response) =>{
-    db.collection('players').find().sort({score: 1}).toArray()
-    .then(data => {
-        response.render('index.ejs', { info: data})
-    })
-    .catch(error => console.error(error))
-})
-//CREATE /addPlayer 
-app.post('/addPlayer', (request, response) => {
-    db.collection('players').insertOne({playerName: request.body.playerName, score: 0})
-    .then(result => {
-        console.log('New player added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
-
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
@@ -121,7 +154,16 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
 
     app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
-}) */
+}) 
+
+//DELETE: deleteOne
+app.delete('/deletePlayer', (request, response) => {
+    db.collection('players').deleteOne({playerName: request.body.playerNameCur || ''})
+    .then(result => {
+        console.log('Player deleted')
+        response.json('Player deleted')
+    })
+    .catch(error => console.error(error))*/
 
 
 //Connect to the database before listening MONGOCLIENT
